1c67c60b44a137c844b5d89794aadbd311d9ac03
Etos2 <etos2etos2@gmail.com>
2025-01-18 16:41:10 +1100

    Record Api Start

--- mctc-parser/src/lib.rs
+++ mctc-parser/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, ops::RangeInclusive};
+use std::{error::Error, io::{Read, Write}, ops::RangeInclusive};
 
 pub mod data;
 pub mod error;
@@ -21,14 +21,25 @@ impl Default for DefaultOptions {
     }
 }
 
-pub trait WriteRecord<E: From<std::io::Error>> {
+pub trait Record {
     fn type_id(&self) -> u64;
     fn length(&self) -> usize;
-    fn write(&self, wtr: impl Write) -> Result<(), E>;
 }
 
-pub trait FromRecord {}
+pub trait WriteRecord<E: Error>: Record {
+    fn write_into(&self, wtr: impl Write) -> Result<(), E>;
+}
+
+pub trait ReadRecord<E: Error>: Record {
+    fn read_from(rdr: impl Read) -> Result<Self, E> where Self: Sized;
+}
 
 pub trait Codec {
     const NAME: &'static str;
+    type Err: Error;
+    type Rec: Record + WriteRecord<Self::Err> + ReadRecord<Self::Err>;
+
+    fn codec_id(&self) -> u64;
+    fn write_record(&mut self, wtr: impl Write, rec: &Self::Rec) -> Result<(), Self::Err>;
+    fn read_record(&mut self, rdr: impl Read) -> Result<Self::Rec, Self::Err>;
 }
